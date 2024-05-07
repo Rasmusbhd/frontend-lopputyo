295bfbb6185fd7e3a502ab4a99764f464650b2ee
Rasmusbhd <bhd466@myy.haaga-helia.fi>
2024-05-07 17:04:50 +0300

    Valmis lopputyo

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -5,7 +5,6 @@ const Error = () => {
         <div className="error-page">
             <h1>404 - Page Not Found</h1>
             <p>Sorry, the page you are looking for does not exist.</p>
-            <a href="/" className='go-home-button'>Go back to Home</a>
         </div>
     );
 };
